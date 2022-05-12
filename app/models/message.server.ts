f64b545d1596ed5223eb2c44390fdde857ad8b03
creichart <christian.reichart@outlook.com>
2022-05-12 20:56:19 +0200

    fix decryption

--- app/models/message.server.ts
+++ app/models/message.server.ts
@@ -1,5 +1,5 @@
 import type { Message } from "@prisma/client";
-import { AES } from "crypto-js";
+import { AES, enc } from "crypto-js";
 import { prisma } from "~/db.server"
 
 export async function getMessage(id: string): Promise<Message | null> {
@@ -9,7 +9,7 @@ export async function getMessage(id: string): Promise<Message | null> {
 export async function getDecryptedStringFromMessage(id: string, pw: string): Promise<string | null> {
   const msg = await prisma.message.findUnique({ where: { id } });
   if(msg) {
-    const dec = AES.decrypt(msg.encryptedValue, pw).toString();
+    const dec = AES.decrypt(msg.encryptedValue, pw).toString(enc.Utf8);
     return dec;
   }
   return null;
